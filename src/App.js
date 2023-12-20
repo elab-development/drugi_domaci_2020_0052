77fd6ffff80b5fa1fe6af90abb431ea3504a57b5
Ana Boskovic <anaboskovicmis@gmail.com>
2023-12-20 20:42:42 +0100

    placeholder stranica i router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Meni from "./komponente/Meni";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Pocetna from "./stranice/Pocetna";
+import Faq from "./stranice/Faq";
+import ONama from "./stranice/ONama";
+import Usluge from "./stranice/Usluge";
+import Kontakt from "./stranice/Kontakt";
+import Login from "./stranice/Login";
+import { Container } from 'react-bootstrap';
 
 function App() {
   return (
     <>
-    Sajt
+    
+    <BrowserRouter>
+            <Meni/>
+            <Container>
+            <Routes>
+                <Route path="/" element={<Pocetna/>}/>
+                <Route path="/onama" element={<ONama />}/>
+                <Route path="/usluge" element={<Usluge />}/>
+                <Route path="/faq" element={<Faq/>}/>
+                <Route path="/kontakt" element={<Kontakt />}/>
+                <Route path="/login" element={<Login />}/>
+
+            </Routes>
+            </Container>
+        </BrowserRouter>
+       
     </>
   );
 }
