7762dfd6d218e91235a238d4a2795ffcd889b596
Bogdana Bradic <bogdanabradic8@gmail.com>
2023-12-26 20:40:12 +0100

    Dodata forma za registraciju

--- src/stranice/Login.jsx
+++ src/stranice/Login.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import {Button, Form} from "react-bootstrap";
+import {Button, Col, Form, Row} from "react-bootstrap";
 import useForm from '../hooks/UseForm';
 
 const Login = () => {
 
     const onclick = () => {
         alert('Uspesno ste se ulogovali!');
+        console.log(formData);
+    }
+
+    const onclickRegister = () => {
+        alert('Uspesno ste se registrovali!');
+        console.log(formDataRegister);
     }
 
     const initialState = {
@@ -13,23 +19,55 @@ const Login = () => {
         password: ''
     };
 
+    const initialStateRegister = {
+        nameRegister: '',
+        emailRegister: '',
+        passwordRegister: ''
+    };
+
     const [formData, handleChange] = useForm(initialState);
+
+    const [formDataRegister, handleChangeRegister] = useForm(initialStateRegister);
 
     return (
         <div>
-            <Form>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control onChange={handleChange} name="email" type="email" placeholder="name@example.com" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control onChange={handleChange} name="password" type="password" />
-                </Form.Group>
-                <Button onClick={onclick} variant="primary" type="submit" >Login </Button>
-            </Form>
+            <Row>
+                <h1>Login/Registracija forma</h1>
+            </Row>
+            <Row>
+                <Col>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control onChange={handleChange} name="email" type="email" placeholder="name@example.com" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control onChange={handleChange} name="password" type="password" />
+                        </Form.Group>
+                        <Button onClick={onclick} variant="primary" type="button" >Login </Button>
+                    </Form>
+                </Col>
+                <Col>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput5">
+                            <Form.Label>Ime i prezime</Form.Label>
+                            <Form.Control onChange={handleChangeRegister} name="nameRegister" type="text" placeholder="Ivica i Marica" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput4">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control onChange={handleChangeRegister} name="emailRegister" type="email" placeholder="name@example.com" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control onChange={handleChangeRegister} name="passwordRegister" type="password" />
+                        </Form.Group>
+                        <Button onClick={onclickRegister} variant="primary" type="button" >Registracija </Button>
+                    </Form>
+                </Col>
+            </Row>
         </div>
     );
 };
 
-export default Login;
+export default Login;
