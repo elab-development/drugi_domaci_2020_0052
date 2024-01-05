dbec5a68b083de9bae509954f1b3e8db248dbc20
Bogdana Bradic <bogdanabradic8@gmail.com>
2024-01-05 20:25:15 +0100

    Pocetna i FAQ stranice

--- src/stranice/Faq.jsx
+++ src/stranice/Faq.jsx
@@ -1,10 +1,57 @@
 import React from 'react';
+import {Accordion, Container, Row} from "react-bootstrap";
 
 const Faq = () => {
+
+    const faq = [
+        {
+            id: 1,
+            pitanje: "Da li postoji ograničen broj usluga koje mogu biti istovremeno zatražene?",
+            odgovor: "Ne postoji ograničen broj usluga koje možete zatražiti, ali ne postoji mogućnost izvršavanja više usluga istovremeno."
+        },
+        {
+            id: 2,
+            pitanje: "Da li postoji mogućnost rangiranja hitnosti zahteva za određenim uslugama?",
+            odgovor: "Naravno. Ukoliko postoje određeni zahtevi koje je potrebno obraditi u što kraćem vremenskom periodu, potrebno je to i naglasiti prilikom traženja usluge. U tom slučaju služba će blagovremeno rangirati tražene usluge i pružiti odgovor u što kraćem vremenskom roku."
+        },
+        {
+            id: 3,
+            pitanje: "Da li zahtevane usluge ostaju autorizovane isključivo za potrebe sajta ili će biti dostupne široj javnosti?",
+            odgovor: "Svi vaši zahtevi biće obrađeni i dostupni isključivo u okviru našeg sajta e-uprave. Vaš nalog i registracija služe isključivo radi lakšeg evidentiranja zahtevanih usluga i davanja adekvatnih odgovora."
+        },
+        {
+            id: 4,
+            pitanje: "Kako izvršiti elektronski potpis?",
+            odgovor: "Postavljeni zahtev možete izvršiti putem posebne aplikacije za elektronski potpis. Na samoj aplikaciji imate kratko uputstvo u kome su definisani jasni koraci koji će vam dati mogućnost da napravite lični identifikacioni potpis."
+        },
+        ];
     return (
-        <div>
-            Faq
-        </div>
+        <>
+            <Container>
+                <div className="App-header">
+                    <h1>FAQ</h1>
+                    <p>
+                        odogovori na najcesca pitanja
+                    </p>
+                </div>
+                <Row>
+                    <Accordion defaultActiveKey="0">
+                        {
+                            faq.map((item) => {
+                                return (
+                                    <Accordion.Item eventKey={item.id} key={item.id}>
+                                        <Accordion.Header>{item.pitanje}</Accordion.Header>
+                                        <Accordion.Body>
+                                            {item.odgovor}
+                                        </Accordion.Body>
+                                    </Accordion.Item>
+                                )
+                            })
+                        }
+                    </Accordion>
+                </Row>
+            </Container>
+        </>
     );
 };
 
--- src/stranice/Pocetna.jsx
+++ src/stranice/Pocetna.jsx
@@ -1,10 +1,53 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import axiosInstance from "../axios-instance/axios";
+import {Carousel, Container, Image, Row} from "react-bootstrap";
+import slika1 from "../slike/slider/1.jpeg";
+import slika2 from "../slike/slider/2.jpeg";
+import slika3 from "../slike/slider/4.jpeg";
 
 const Pocetna = () => {
+
     return (
-        <div>
-            Pocetna
-        </div>
+        <>
+            <Container>
+                <div className="App-header">
+                    <h1>Dobrodošli</h1>
+                    <p>
+                        na sajt E-uprave
+                    </p>
+                </div>
+                <Row>
+                    <Carousel className="mt-3" fade >
+                        <Carousel.Item>
+                            <Image src={slika1} fluid={true}/>
+                            <Carousel.Caption>
+                                <h3>Dobrodošli</h3>
+                                <p>Mi vam možemo pomoci</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image src={slika2}  fluid={true}/>
+                            <Carousel.Caption>
+                                <h3>Ne morate vise cekati u redovima</h3>
+                                <p>Sada mozete i online odraditi birokratske usluge</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <Image src={slika3} fluid={true} />
+                            <Carousel.Caption>
+                                <h3>Birokratija na dohvat ruke</h3>
+                                <p>
+                                    Konacno mozete odraditi sve usluge iz udobnosti svog doma
+                                </p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                </Row>
+                <Row>
+                    <p className="mt-3">E-uprava je informativni sajt koji Vam pruža mogućnost da na što brži i efikasniji način dođete do odgovara koja se tiču različitih vrsta usluga. Višečasovno čekanje u opštini i drugim ustanovama možete zameniti jednim klikom na uslugu koja Vas zanima i tako doći do traženog odgovora.  Takođe, u što kraćem vremenskom roku bićete obavešteni i informisani o svim novonastalim promenama. Ukoliko imate bilo kakvih dodatnih pitanja ili problema prilikom traženja određenih usluga, budite slobodni da nas kontaktirate u bilo kom trenutku.</p>
+                </Row>
+            </Container>
+        </>
     );
 };
 
